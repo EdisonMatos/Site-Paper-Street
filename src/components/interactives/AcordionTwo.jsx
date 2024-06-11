3cd641e5d64083d5834c8b857feec9c0b2daed2b
Edison Matos <edison.matos@live.com>
2024-06-11 17:14:48 -0300

    feat: Insere copy em FAQ

--- src/components/interactives/AcordionTwo.jsx
+++ src/components/interactives/AcordionTwo.jsx
@@ -138,6 +138,62 @@ export default function AccordionExpandDefault() {
           </Typography>
         </AccordionDetails>
       </Accordion>
+
+      {/* Accordion 5 */}
+      <Accordion
+        className="mb-[3%]"
+        expanded={expanded === "panel5"}
+        onChange={handleChange("panel5")}
+        style={getPanelStyle("panel5")}
+      >
+        <AccordionSummary
+          className=" h-[76px]"
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel5-content"
+          id="panel5-header"
+        >
+          <Typography>
+            <h1 className="font-bold font-secondFont">
+              {content.texts.faq.questions.question5.question}
+            </h1>
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            <p className="font-secondFont text-paragraph4 text-paragraphLight ">
+              {content.texts.faq.questions.question5.answer}
+            </p>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      {/* Accordion 6 */}
+      <Accordion
+        className="mb-[3%]"
+        expanded={expanded === "panel6"}
+        onChange={handleChange("panel6")}
+        style={getPanelStyle("panel6")}
+      >
+        <AccordionSummary
+          className=" h-[76px]"
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel6-content"
+          id="panel6-header"
+        >
+          <Typography>
+            <h1 className="font-bold font-secondFont">
+              {content.texts.faq.questions.question6.question}
+            </h1>
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            <p className="font-secondFont text-paragraph4 text-paragraphLight ">
+              {content.texts.faq.questions.question6.answer}
+            </p>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
     </div>
   );
 }
