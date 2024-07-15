4f01c3d149b451868c647103276b710bbd9e0a00
Gabriel <gabrielsouza65115@gmail.com>
2024-07-15 09:44:24 -0300

    feat: Insere o aria-label nos ícones da Navbar

--- src/components/sectionElements/ListGroup.jsx
+++ src/components/sectionElements/ListGroup.jsx
@@ -157,6 +157,7 @@ export default function ListGroup() {
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 class="lucide lucide-instagram"
+                aria-label="Botão do Instagram"
               >
                 <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
                 <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
@@ -179,6 +180,7 @@ export default function ListGroup() {
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 class="lucide lucide-facebook"
+                aria-label="Botão de Facebook"
               >
                 <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
               </svg>
@@ -199,6 +201,7 @@ export default function ListGroup() {
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 class="lucide lucide-linkedin"
+                aria-label="Botão do LinkedIn"
               >
                 <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
                 <rect width="4" height="12" x="2" y="9" />
