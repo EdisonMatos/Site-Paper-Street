f6c9b18e862228db117dfed4c41de0dc6f562324
Gabriel <gabrielsouza65115@gmail.com>
2024-08-27 17:03:26 -0300

    feat: Insere barrinhas abaixo das imagens que sinalizam a troca de imagens

--- src/components/interactives/TestimonialsDiv.jsx
+++ src/components/interactives/TestimonialsDiv.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Carousel } from "primereact/carousel";
 import "../../index.css";
 
 export default function TestimonialsDiv({ children }) {
+  const [activeIndex, setActiveIndex] = useState(0);
+
   const responsiveOptions = [
     {
       breakpoint: "2500px",
       numVisible: 2,
       numScroll: 2,
-    }, // organiza em 4 por clique e passa os 4 de uma só vez
+    },
     {
       breakpoint: "1199px",
       numVisible: 2,
@@ -28,10 +30,14 @@ export default function TestimonialsDiv({ children }) {
 
   const itemTemplate = (item) => {
     return (
-      <div className="border-1 rounded text-center py-5 px-3 h-full w-auto">
+      <div className="border-1 surface-border rounded text-center py-5 px-3 h-full w-auto">
         <div className="flex justify-center items-center h-full">{item}</div>
       </div>
     );
+  };
+
+  const onPageChange = (e) => {
+    setActiveIndex(e.page);
   };
 
   return (
@@ -42,13 +48,25 @@ export default function TestimonialsDiv({ children }) {
         numScroll={1}
         responsiveOptions={responsiveOptions}
         className="text-black"
-        circular
+        circular={true}
         autoplayInterval={8000}
         itemTemplate={itemTemplate}
+        onPageChange={onPageChange}
         showNavigators={true}
         prevIcon={<span style={{ color: "black", fontSize: "200%" }}>❮</span>}
         nextIcon={<span style={{ color: "black", fontSize: "200%" }}>❯</span>}
       />
+      <div className="flex justify-center mt-3">
+        {children.map((_, index) => (
+          <span
+            key={index}
+            className={`mx-1 w-4 h-1 ${
+              index === activeIndex ? "bg-blue-500" : "bg-gray-300"
+            }`}
+            style={{ transition: "background-color 0.3s" }}
+          ></span>
+        ))}
+      </div>
     </div>
   );
 }
