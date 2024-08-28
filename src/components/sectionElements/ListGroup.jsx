c1cef1959afb02e67d39d5f4fae7dd7b856da3b8
Gabriel <gabrielsouza65115@gmail.com>
2024-08-28 09:15:35 -0300

    fix: Insere a propriedade aria-label nos botões da Navbar

--- src/components/sectionElements/ListGroup.jsx
+++ src/components/sectionElements/ListGroup.jsx
@@ -124,7 +124,7 @@ export default function ListGroup() {
           ></div>
         </Link>
       </li>
-      
+
       <li>
         <div className="flex gap-[10px] items-center">
           <div className={scrolling ? "invert" : ""}>
@@ -153,6 +153,7 @@ export default function ListGroup() {
             <IconButton
               icon={
                 <svg
+                  aria-label="Botão de Instagram"
                   xmlns="http://www.w3.org/2000/svg"
                   width="20"
                   height="20"
@@ -179,6 +180,7 @@ export default function ListGroup() {
             <IconButton
               icon={
                 <svg
+                  aria-label="Botão de Facebook"
                   xmlns="http://www.w3.org/2000/svg"
                   width="20"
                   height="20"
@@ -203,6 +205,7 @@ export default function ListGroup() {
             <IconButton
               icon={
                 <svg
+                  aria-label="Botão de Linkedin"
                   xmlns="http://www.w3.org/2000/svg"
                   width="20"
                   height="20"
