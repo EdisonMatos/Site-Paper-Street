71bb7e15cf73f582602ad320785d81a44ebeb98d
Gabriel <gabrielsouza65115@gmail.com>
2024-08-29 15:33:58 -0300

    fix: Estiliza os textos da Sessão de Steps de acordo com o padrão

--- src/components/cards/HowItWorksCard.jsx
+++ src/components/cards/HowItWorksCard.jsx
@@ -8,11 +8,13 @@ export default function HowItWorksCard({
 }) {
   const Content = (
     <div className="flex flex-col items-center justify-center transition desktop1:items-start font-mainFont desktop1:hover:scale-110 h-auto">
-      <div className="bg-primary mb-[32px] p-[25px] flex justify-center items-center rounded-full h-[48px] w-[48px] text-yellow-400  text-title1 font-semibold">
+      <div className="bg-yellow-500 mb-[32px] p-[25px] flex justify-center items-center rounded-full h-[48px] w-[48px] text-primary  text-title1 font-semibold">
         {number}
       </div>
-      <h1 className="text-[20px] text-lighter font-bold mb-[24px]">{title}</h1>
-      <p className="text-[16px] text-lighter opacity-70 text-center desktop1:text-left w-full">
+      <h1 className="text-[20px] text-lighte font-secondFont font-bold mb-[24px]">
+        {title}
+      </h1>
+      <p className="text-[16px] font-secondFont text-lighter opacity-70 text-center desktop1:text-left w-full">
         {description}
       </p>
     </div>
@@ -20,11 +22,13 @@ export default function HowItWorksCard({
 
   const ContentStatic = (
     <div className="flex flex-col items-center justify-center tablet1:w-[45%] transition desktop1:items-start font-mainFont desktop1:hover:scale-110 h-auto">
-      <div className="bg-primary mb-[32px] p-[25px] flex justify-center items-center rounded-full h-[48px] w-[48px] text-yellow-400 text-title1 font-semibold">
+      <div className="bg-yellow-500 mb-[32px] p-[25px] flex justify-center items-center rounded-full h-[48px] w-[48px] text-primary text-title1 font-semibold">
         {number}
       </div>
-      <h1 className="text-[20px] text-lighter font-bold mb-[24px]">{title}</h1>
-      <p className="text-[16px] text-lighter opacity-70 text-center desktop1:text-left w-full">
+      <h1 className="text-[20px] text-lighter font-secondFont font-bold mb-[24px]">
+        {title}
+      </h1>
+      <p className="text-[16px] font-secondFont text-lighter opacity-70 text-center desktop1:text-left w-full">
         {description}
       </p>
     </div>
