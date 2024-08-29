c930d54b15a3598c633f215ce940e9457b0924bd
Gabriel <gabrielsouza65115@gmail.com>
2024-08-29 08:04:15 -0300

    fix: Muda estrutura da Sessão About para inserir minitag no SectionHeader

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,85 +1,106 @@
-import React, { useState } from "react";
 import Paragraphs from "../sectionElements/Paragraphs";
 import SectionArea from "../sectionElements/SectionArea";
-import SectionTitles from "../sectionElements/SectionTitles";
 import SectionWrapper from "../sectionElements/SectionWrapper";
 import MotionDivDownToUp from "../animation/MotionDivDownToUp";
-import { Dialog } from "primereact/dialog";
-import "primereact/resources/themes/saga-blue/theme.css";
-import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";
+import React, { useState } from "react";
+// import { Dialog } from "primereact/dialog";
+// import "primereact/resources/themes/saga-blue/theme.css";
+// import "primereact/resources/primereact.min.css";
+// import "primeicons/primeicons.css";
 import content from "../../content/content";
-import ButtonWithIcon from "../interactives/ButtonWithIcon";
+// import Button from "../interactives/Button";
+import links from "../../content/links";
+import SectionHeader from "../sectionElements/SectionHeader";
+// import imgPoints from "../../assets/imgs/about/points.png";
+// import AboutFading from "../sectionElements/AboutFading";
+
+const whatsappContactLink = `https://wa.me/` + `${links.ctaWhatsapp}`;
 
 export default function About() {
   const [visible, setVisible] = useState(false);
   const [modalContent, setModalContent] = useState("");
   const [modalTitle, setModalTitle] = useState("");
 
-  const onClick = () => {
-    setModalTitle("Sobre mim");
-    setModalContent(
-      <p>
-        Creci: 11798/PE<br></br> Ensino Superior: Administração - Facape
-        <br></br>
-        <br></br> Olá, me chamo Rita Almeida, sou corretora de imóveis há mais
-        de 10 anos e sou formada em Administração há 30. Decidi entrar no ramo
-        quando um colega me fez um elogio: "Rita, você é tão comunicativa,
-        deveria ser corretora de imóveis." Eu ainda fiquei receosa: "Ah, isso
-        não é para mim," mas decidi arriscar. Fiz o curso quando minha filha
-        ainda era pequena e não me arrependo da decisão que tomei.<br></br>
-        <br></br> Com a minha formação em Administração, percebi rapidamente que
-        muitas das habilidades adquiridas durante o curso eram essenciais no
-        mercado imobiliário. A capacidade de negociar, organizar finanças e
-        entender os fundamentos econômicos são apenas algumas das competências
-        que me ajudaram a destacar-me na profissão.<br></br>
-        <br></br> Nos primeiros anos, equilibrar a vida pessoal com a
-        profissional foi um desafio, especialmente sendo mãe de uma criança
-        pequena. Contudo, sempre busquei manter um alto padrão de ética e
-        dedicação, o que me permitiu construir uma reputação sólida e uma base
-        de clientes fiéis.<br></br>
-        <br></br> Ao longo dos anos, vi muitas mudanças no setor imobiliário. A
-        tecnologia transformou a maneira como interagimos com clientes e como os
-        imóveis são apresentados e comercializados. Adaptar-me a essas mudanças
-        foi essencial, e sempre procurei estar atualizada, seja através de
-        cursos de formação continuada ou participando de seminários e workshops.
-        <br></br>
-        <br></br> Hoje, posso dizer que minha carreira é uma grande paixão. Cada
-        dia traz um novo desafio e a oportunidade de ajudar alguém a encontrar o
-        lugar ideal para chamar de lar. Continuo empregando as habilidades de
-        administração no meu dia a dia, gerenciando não apenas propriedades, mas
-        também sonhos e expectativas.
-      </p>
-    );
-    setVisible(true);
-  };
+  // const onClick = () => {
+  //   setModalTitle(content.texts.about.titleModal);
+  //   setModalContent(
+  //     <p>
+  //       {content.texts.about.modal}
+  //       <br />
+  //       <p>Quer saber mais sobre nós? 👇</p>
+  //       <br />
+  //       <div>
+  //         <Button
+  //           aria-label={content.texts.hero.ctaButtonAriaLabel}
+  //           label={content.texts.hero.ctaButtonText}
+  //           buttonLink={whatsappContactLink}
+  //           animation={false}
+  //           className="hover:scale-105"
+  //           icon={
+  //             <svg
+  //               xmlns="http://www.w3.org/2000/svg"
+  //               width="24"
+  //               height="24"
+  //               fill="currentColor"
+  //               className="bi bi-whatsapp"
+  //               viewBox="0 0 16 16"
+  //             >
+  //               <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
+  //             </svg>
+  //           }
+  //         />
+  //       </div>
+  //     </p>
+  //   );
+  //   setVisible(true);
+  // };
 
   return (
-    <SectionArea id={"about"} className="">
-      <SectionWrapper className="flex flex-col desktop1:flex-row gap-[40px] desktop1:gap-0 desktop1:justify-between">
-        <MotionDivDownToUp className="w-[100%] desktop1:w-[415px] desktop2:w-[485px]">
+    <SectionArea id="about" className="bg-bgSectionDark" paddingbot={false}>
+      <SectionWrapper className="flex flex-col desktop1:flex-row gap-[40px] desktop1:gap-x-[40px] desktop2:gap-0 desktop1:justify-between">
+        <MotionDivDownToUp className=" w-[100%] desktop1:w-[415px] desktop2:w-[485px] flex justify-center">
           <div
             style={{
               backgroundImage: `url(${content.texts.about.imagem.img})`,
             }}
-            className="bg-bottom bg-no-repeat bg-cover h-[290px] desktop1:h-[650px] rounded-2xl"
-          />
+            className="relative bg-center bg-no-repeat bg-cover h-[290px] w-full desktop1:h-[650px] rounded-2xl"
+          >
+            {/* <img
+              alt="Imagem de efeito pontilhado"
+              src={imgPoints}
+              className="absolute opacity-75 phone1:right-[-10px] phone1:top-[20px] desktop1:right-[-40px] desktop1:top-[40px]"
+            ></img> */}
+          </div>
+          {/* <img
+            src={content.texts.about.imagem.img}
+            alt="Imagem de uma praia de Arraial"
+            className=""
+          /> */}
         </MotionDivDownToUp>
-        <div className="desktop1:w-[450px] desktop2:w-[570px]">
+
+        <div className="desktop1:w-[550px] desktop2:w-[570px] ">
+          <SectionHeader
+            className="text-center"
+            miniTitle={content.texts.about.miniTag}
+            sectionHeaderTitle={content.texts.about.title}
+            sectionHeaderSubtitle={content.texts.about.subtitle}
+            color=""
+            type="article"
+          />
           <MotionDivDownToUp>
-            <SectionTitles className="mb-[18px] desktop1:mb-[26px]">
-              {content.texts.about.title}
-            </SectionTitles>
-          </MotionDivDownToUp>
-          <MotionDivDownToUp>
-            <Paragraphs className="mb-[0px] desktop1:mb-[0px]">
+            <Paragraphs className="text-black text-opacity-80">
+              {/* <AboutFading /> */}
               {content.texts.about.paragraph}
             </Paragraphs>
-          </MotionDivDownToUp>
-          {/* <MotionDivDownToUp>
-            <ButtonWithIcon
-              label="Leia mais"
+            {/* Início Botão de saiba mais abrindo modal */}
+
+            {/* <Button
+              className="mt-[48px]"
+              label="Continuar lendo"
               onClick={onClick}
+              removeAnchor={true}
+              removeTarget={true}
+              animation={false}
               icon={
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -97,20 +118,108 @@ export default function About() {
                   <path d="M2 12H22" />
                 </svg>
               }
-            />
-          </MotionDivDownToUp> */}
+            /> */}
+
+            {/* Fim Botão de saiba mais abrindo modal */}
+          </MotionDivDownToUp>
+
+          {/* Início Botões das redes sociais */}
+
+          {/* <div className=" flex flex-col gap-[16px] w-[100%] tablet1:w-[60%] desktop1:w-[80%] desktop2:w-[60%]">
+            <MotionDivDownToUp>
+              <Button
+                label="Me siga no Instagram"
+                className="w-[75%]"
+                buttonLink={links.socialMedia.instagram}
+                textclassName="text-paragraph3"
+                size="small"
+                icon={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="lucide lucide-instagram"
+                  >
+                    <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+                    <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
+                    <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
+                  </svg>
+                }
+              />
+            </MotionDivDownToUp>
+            <MotionDivDownToUp>
+              <Button
+                label="Me siga no Facebook"
+                className="w-[75%]"
+                buttonLink={links.socialMedia.facebook}
+                textclassName="text-paragraph3"
+                size="small"
+                icon={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="lucide lucide-facebook"
+                  >
+                    <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
+                  </svg>
+                }
+              />
+            </MotionDivDownToUp>
+            <MotionDivDownToUp>
+              <Button
+                label="Me siga no Linkedin"
+                className="w-[75%]"
+                buttonLink={links.socialMedia.linkedin}
+                textclassName="text-paragraph3"
+                size="small"
+                icon={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="lucide lucide-linkedin"
+                  >
+                    <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
+                    <rect width="4" height="12" x="2" y="9" />
+                    <circle cx="4" cy="4" r="2" />
+                  </svg>
+                }
+              />
+            </MotionDivDownToUp>
+          </div> */}
+
+          {/* Fim Botões das redes sociais */}
         </div>
       </SectionWrapper>
-      <Dialog
-        className=" font-secondFont"
+      {/* <Dialog
+        className="font-secondFont"
         header={modalTitle}
         visible={visible}
         onHide={() => setVisible(false)}
         style={{ width: "50vw" }}
-        breakpoints={{ "960px": "75vw", "641px": "90vw" }}
+        breakpoints={{ "4000px": "60vw", "1024px": "70vw", "641px": "85vw" }}
       >
         <p className="m-0 ">{modalContent}</p>
-      </Dialog>
+      </Dialog> */}
     </SectionArea>
   );
 }
