846f5ca08627147eefd1465b44734849c6e078f6
Gabriel <gabrielsouza65115@gmail.com>
2024-09-18 15:46:16 -0300

    fix: Estiliza os espaços na Hero

--- src/components/sectionElements/ScrollDownIndicator.jsx
+++ src/components/sectionElements/ScrollDownIndicator.jsx
@@ -6,7 +6,7 @@ export default function ScrollDownIndicator() {
     <div className="flex justify-center phone2:flex">
       <MotionDivRightToLeft>
         <div className="flex flex-col items-center">
-          <p className="mb-[22px] text-white desktop1:mt-[20px] desktop3:pt-[20px] opacity-80 font-secondFont">
+          <p className="mb-[22px] text-white mt-[40%] desktop1:mt-[50%] opacity-80 font-secondFont">
             Deslize para baixo
           </p>
           <div className="scrollDown">
