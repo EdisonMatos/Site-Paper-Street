bd3b426cbc76fc882f27397c430f3548a0140e59
Edison Matos <edison.matos@live.com>
2024-11-25 15:53:55 -0300

    feat: Cria rota

---
+++ src/pages/Contrato.jsx
@@ -0,0 +1,15 @@
+import SectionArea from "../components/sectionElements/SectionArea";
+import SectionWrapper from "../components/sectionElements/SectionWrapper";
+
+export default function Contrato() {
+  return (
+    <div>
+      <SectionArea>
+        <SectionWrapper>
+          <div className="caixadetermos"></div>
+          <div className="formcombotoesesubmit"></div>
+        </SectionWrapper>
+      </SectionArea>
+    </div>
+  );
+}
